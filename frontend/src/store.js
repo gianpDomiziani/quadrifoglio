a61c818a3bbf809c169f29a7bf59da2e156ce29d
gianpDomiziani <gpdomiziani@gmail.com>
2020-11-06 13:04:30 +0100

    redux started

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import data from './data';
+import { productListReducer } from './reducers/productReducers';
+
+const initialState = {};
+const reducer = combineReducers({
+    productList: productListReducer,
+})
+const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer, 
+    initialState, 
+    composeEnhacer(applyMiddleware(thunk)));
+
+export default store;
+
