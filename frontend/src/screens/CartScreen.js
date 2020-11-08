024b2bbd229d7ac6db68b376180b2de9538a2388
gianpDomiziani <gpdomiziani@gmail.com>
2020-11-08 18:22:17 +0100

    cart action added. (Remove items)

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../actions/cartActions';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart } from '../actions/cartActions';
+import MessageBox from '../components/MessageBox';
+
+import Button from '@material-ui/core/Button';
+
 
 
 export default function CartScreen(props) {
@@ -8,6 +13,8 @@ export default function CartScreen(props) {
     const qty = props.location.search
     ? Number(props.location.search.split('=')[1])
     : 1;
+    const cart = useSelector(state => state.cart);
+    const { cartItems } = cart;
     const dispatch = useDispatch();
     useEffect(() => {
         if(productId) {
@@ -15,12 +22,90 @@ export default function CartScreen(props) {
         }
 
     }, [dispatch, productId, qty])
+    const removeFromCartHandler = (id) => {
+        // delete action
+        dispatch(removeFromCart(id));
+    };
+
+    const checkoutHandler = () => (
+        props.history.push('/signin?redirect=shipping')
+    )
     return (
-        <div>
-            <h1>Carrello</h1>
-            <p>
-                Aggiunto al carrello: Prodotto ID: {productId} Qty: {qty}.
-            </p>
+        <div className="row top">
+        <Button><Link to='/'>Ritorna agli acquisti</Link></Button>
+        <div className="col-2">
+          <h1>Elementi nel carrello</h1>
+          {cartItems.length === 0 ? (
+            <MessageBox>
+              Carrello vuoto. <Button><Link to="/">Continua gli acquisti</Link></Button>
+            </MessageBox>
+          ) : (
+            <ul>
+              {cartItems.map((item) => (
+                <li key={item.product}>
+                  <div className="row">
+                    <div>
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="small"
+                      ></img>
+                    </div>
+                    <div className="min-30">
+                      <Button><Link to={`/product/${item.product}`}>{item.name}</Link></Button>
+                    </div>
+                    <div>
+                      <select
+                        value={item.qty}
+                        onChange={(e) =>
+                          dispatch(
+                            addToCart(item.product, Number(e.target.value))
+                          )
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                    <div>€{item.price}</div>
+                    <div>
+                      <Button
+                        onClick={() => removeFromCartHandler(item.product)}
+                      >
+                        elimina prodotto
+                      </Button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <h2>
+                  Totale: ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : $
+                  {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+                </h2>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  onClick={checkoutHandler}
+                  className="primary block"
+                  disabled={cartItems.length === 0}
+                >
+                  Prosegui con l'acquisto
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     );
-};
+  }
