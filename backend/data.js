cc6d2991f46d415c2747ae159930ebe54aad4810
gianpDomiziani <gpdomiziani@gmail.com>
2020-11-09 16:19:43 +0100

    mongodb started: users and products collections inserted.

--- backend/data.js
+++ backend/data.js
@@ -1,12 +1,28 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
 
+    users: [
+        {
+            name: 'GianP',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true,
+        },
+        {
+            name: 'Viro',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('1784', 8),
+            isAdmin: false,
+        }
+    ],
 
     products: [
         {
-            _id: '1',
-            name: 'Vitamine C',
+            name: 'VitaC',
             category: 'Alimenti',
             image: '/images/p1.jpg',
+            brand: 'naturaSi',
             price: 18,
             rating: 4.5,
             numReviews: 12,
@@ -14,10 +30,10 @@ const data = {
             countInStock: 10
         },
         {
-            _id: '2',
             name: 'Ginko',
             category: 'Alimenti',
             image: '/images/p2.jpg',
+            brand: 'naturaSi',
             price: 18,
             rating: 3.5,
             numReviews: 18,
@@ -25,9 +41,9 @@ const data = {
             countInStock: 10
         },
         {
-            _id: '3',
             name: 'Vitamine B',
             category: 'Alimenti',
+            brand: 'naturaSi',
             image: '/images/p1.jpg',
             price: 18,
             rating: 4.5,
@@ -36,9 +52,9 @@ const data = {
             countInStock: 7
         },
         {
-            _id: '4',
             name: 'Rodiola',
             category: 'Alimenti',
+            brand: 'naturaSi',
             image: '/images/p2.jpg',
             price: 18,
             rating: 2.0,
@@ -47,9 +63,9 @@ const data = {
             countInStock: 9
         },
         {
-            _id: '5',
-            name: 'Vitamine C',
+            name: 'Immune C',
             category: 'Alimenti',
+            brand: 'naturaSi',
             image: '/images/p1.jpg',
             price: 18,
             rating: 3.0,
@@ -58,9 +74,9 @@ const data = {
             countInStock: 9
         },
         {
-            _id: '6',
             name: 'Vitamine Z',
             category: 'Alimenti',
+            brand: 'naturaSi',
             image: '/images/p2.jpg',
             price: 38,
             rating: 4.0,
