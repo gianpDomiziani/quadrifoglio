95497bcc736396cdfb46e333706487fed3a4876c
gianpDomiziani <gpdomiziani@gmail.com>
2020-11-11 13:58:31 +0100

    HomeList component implemented.

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -8,10 +8,10 @@ const userRouter = express.Router();
 userRouter.get(
     '/seed',
     expressAsyncHandler(async (req, res) => {
-    //await User.deleteMany({});
-    const createdUsers = await User.insertMany(data.users);
-    res.send({createdUsers});
-})
+        //await User.deleteMany({});
+        const createdUsers = await User.insertMany(data.users);
+        res.send({ createdUsers });
+    })
 );
 
 export default userRouter
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,65 +1,37 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Product from '../components/Product';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import HomeList from '../components/HomeList';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productActions';
 
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import { CallMissedSharp } from '@material-ui/icons';
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
-  gridList: {
-    flexWrap: 'nowrap',
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: 'translateZ(0)',
-  },
-  title: {
-    color: theme.palette.primary.light,
-  },
-  titleBar: {
-    background:
-      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  },
-}));
-
 export default function HomeScreen() {
   const dispatch = useDispatch();
-  const productList = useSelector( (state) => state.productList);
+  const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
-    useEffect(() => {
-      dispatch(listProducts());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
-      <div className="row center">
-          {loading ? (
+    <div className="row center">
+      {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
-        <div className="row center">
-          <div className="row center">
-           {products.map((product) => (
-            <Product key={product._id} product={product}></Product>
-          ))}
-        </div>
-          ))
-        </div>
-      )}
-      </div>
+            <div className="row center">
+              <div className="row center">
+                <HomeList products={products}></HomeList>
+              </div>
+              <div className="row center">
+                {products.map((product) => (
+                  <Product key={product._id} product={product}></Product>
+                ))}
+              </div>
+            </div>
+          )}
+    </div>
   );
 }
 
