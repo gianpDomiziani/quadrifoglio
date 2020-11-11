ff2522bbac6b459b78e449f1c82d614ade8cddf5
gianpDomiziani <gpdomiziani@gmail.com>
2020-11-11 14:32:00 +0100

    nothing.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,54 +6,6 @@ import { Link } from 'react-router-dom'
 export default function Product(props) {
   //const classes = useStyles();
   const { product } = props;
-<<<<<<< HEAD
-  return (
-    <div key={product._id} className="card">
-      <Link to={`/product/${product._id}`}>
-        <img className="medium" src={product.image} alt={product.name} />
-      </Link>
-      <div className="card-body">
-        <Link to={`/product/${product._id}`}>
-          <h2>{product.name}</h2>
-        </Link>
-        <Rating
-          rating={product.rating}
-          numReviews={product.numReviews}
-        ></Rating>
-        <div className="price">€{product.price}</div>
-      </div>
-    </div>
-  );
-}
-
-
-
-
-
-/**
- * The example data is structured as follows:
- *
- * import image from 'path/to/image.jpg';
- * [etc...]
- *
- * const tileData = [
- *   {
- *     img: image,
- *     title: 'Image',
- *     author: 'author',
- *   },
- *   {
- *     [etc...]
- *   },
- * ];
- */
-
-/*export default function Product(props) {
-  const {product} = props;
-  const classes = useStyles();
-
-=======
->>>>>>> style-dev
   return (
     <div key={product._id} className="card">
       <Link to={`/product/${product._id}`}>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,10 +6,6 @@ import HomeList from '../components/HomeList';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productActions';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> style-dev
 export default function HomeScreen() {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
@@ -26,12 +22,9 @@ export default function HomeScreen() {
       ) : (
             <div className="row center">
               <div className="row center">
-<<<<<<< HEAD
-=======
                 <HomeList products={products}></HomeList>
               </div>
               <div className="row center">
->>>>>>> style-dev
                 {products.map((product) => (
                   <Product key={product._id} product={product}></Product>
                 ))}
