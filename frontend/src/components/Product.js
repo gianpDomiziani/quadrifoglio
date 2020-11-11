b96ec2cbf42e13cd36411d279781cb13b663a818
gianpDomiziani <gpdomiziani@gmail.com>
2020-11-11 11:08:06 +0100

    clean up

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,23 +2,107 @@ import React from 'react'
 import Rating from './Rating';
 import {Link} from 'react-router-dom'
 
+
 export default function Product(props) {
-    const {product} = props;
+  //const classes = useStyles();
+  const {product} = props;
     return (
         <div key={product._id} className="card">
-                  <Link to={`/product/${product._id}`}>
-                    <img className="medium" src={product.image} alt={product.name}/>
-                </Link>
-                <div className="card-body">
-                  <Link to={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </Link>
-                  <Rating
-                  rating={product.rating}
-                  numReviews={product.numReviews}
-                  ></Rating>
-                  <div className="price">€{product.price}</div>
-                </div>
-              </div>
-    );
+            <Link to={`/product/${product._id}`}>
+            <img className="medium" src={product.image} alt={product.name}/>
+            </Link>
+          <div className="card-body">
+            <Link to={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+            </Link>
+            <Rating
+            rating={product.rating}
+            numReviews={product.numReviews}
+            ></Rating>
+            <div className="price">€{product.price}</div>
+            </div>
+          </div>
+          );
+        }
+
+
+
+
+
+/**
+ * The example data is structured as follows:
+ *
+ * import image from 'path/to/image.jpg';
+ * [etc...]
+ *
+ * const tileData = [
+ *   {
+ *     img: image,
+ *     title: 'Image',
+ *     author: 'author',
+ *   },
+ *   {
+ *     [etc...]
+ *   },
+ * ];
+ */
+
+/*export default function Product(props) {
+  const {product} = props;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList className={classes.gridList} cols={2.5}>
+          <GridListTile key={product._id}>
+            <img src={product.image} alt={product.name} />
+            <GridListTileBar
+              title={product.name}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton aria-label={`star ${product.name}`}>
+                  <StarBorderIcon className={classes.title} />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))
+      </GridList>
+    </div>
+  );
 }
+
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import { CallMissedSharp } from '@material-ui/icons';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+}));
+*/
