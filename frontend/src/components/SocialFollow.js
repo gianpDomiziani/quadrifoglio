3ae07abf403e43051ebce9b991566bffe5d277d9
gianpDomiziani <gpdomiziani@gmail.com>
2020-11-22 23:33:15 +0100

    html fixed.

--- frontend/src/components/SocialFollow.js
+++ frontend/src/components/SocialFollow.js
@@ -10,21 +10,22 @@ import {
 export default function SocialFollow() {
     return (
         <div className='social-container'>
-            <a href='https://www.youtube.com' target="_blank"
+            <a href='https://www.youtube.com' target="_blank" rel="noreferrer"
                 className="youtube social">
                 <FontAwesomeIcon icon={faYoutube} size='2x' />
             </a>
             <a href="https://www.facebook.com"
-                target="_blank"
+                target="_blank" rel="noreferrer"
                 className="facebook social">
                 <FontAwesomeIcon icon={faFacebook} size="2x" />
             </a>
-            <a href="https://www.twitter.com" target="_blank"
+            <a href="https://www.twitter.com" target="_blank" rel="noreferrer"
                 className="twitter social">
                 <FontAwesomeIcon icon={faTwitter} size="2x" />
             </a>
             <a href="https://www.instagram.com"
                 target="_blank"
+                rel="noreferrer"
                 className="instagram social">
                 <FontAwesomeIcon icon={faInstagram} size="2x" />
             </a>
